292d04f7e5206aa65b30bb88bba32e9bc9831035
Unos <129419169+shoc71@users.noreply.github.com>
2023-05-01 11:30:49 -0400

    Update main.py

--- dont stop now/main.py
+++ dont stop now/main.py
@@ -1,4 +1,44 @@
 import pygame
 pygame.init()
 
-# more code to be added soon
+class Program:
+
+    def __init__(self) -> None:
+        self.running = True
+        pass
+    
+    def processInput(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+                break
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.running = False
+                    break
+                elif event.key == pygame.K_RIGHT:
+                    pass
+                elif event.key == pygame.K_LEFT:
+                    pass
+                elif event.key == pygame.K_DOWN:
+                    pass
+                elif event.key == pygame.K_UP:
+                    pass
+
+    def update(self):
+        pass
+
+    def render(self):
+
+        pygame.display.update()
+
+    def run(self):
+        while self.running:
+            self.processInput()
+            self.update()
+            self.render()
+        pass
+
+program = Program()
+if __name__ == "__main__":
+    program.run()
