f120b3d5041af6ba10ce9742415d128c913c0f20
slysnivy <47337807+slysnivy@users.noreply.github.com>
2023-05-01 13:50:48 -0400

    Added the Scene class
    The scene class will be the dynamic processes in the game by switching between game states such as changing between the menu, the options and levels. It'll be cleaner to have a dedicated scene class with it's own subclasses for the different game states than to include it in program (as program is only in charge of running the game).
    In the current version, the game only consists of switching between two background colors by pressing "w".
    Please refer to updates.txt for a more detailed description of changes in v0.0.2.

--- dont stop now/main.py
+++ dont stop now/main.py
@@ -1,44 +1,122 @@
 import pygame
-pygame.init()
+
+
+class Scene:
+    def __init__(self):
+        """
+        self.this_scene will tell the current scene it's on at that moment.
+        Currently, it's set to itself, which means the
+        current scene is this one.
+        """
+        self.this_scene = self
+        self.run_scene = True
+
+    def input(self, pressed, held):
+        # this will be overridden in subclasses
+        pass
+
+    def update(self):
+        # this will be overridden in subclasses
+        pass
+
+    def render(self, screen):
+        # this will be overridden in subclasses
+        pass
+
+    def change_scene(self, next_scene):
+        """
+        This function is used in the main pygame loop
+        """
+        self.this_scene = next_scene
+
+    def close_game(self):
+        """
+        Set the current scene to nothing and is used to stop the game.
+        """
+        self.change_scene(None)
+
+
+class MenuScene(Scene):
+
+    def __init__(self):
+        """
+        Set the current scene to this scene by passing this classes self to
+        initialize it.
+        """
+        Scene.__init__(self)
+
+    def input(self, pressed, held):
+        for every_key in pressed:
+            if every_key == pygame.K_w:
+                self.change_scene(LevelScene())
+
+    def update(self):
+        pass
+
+    def render(self, screen):
+        screen.fill((181, 60, 177))
+
+
+class LevelScene(Scene):
+
+    def __init__(self):
+        """
+        Set the current scene to this scene by passing this classes self to
+        initialize it.
+        """
+        Scene.__init__(self)
+
+    def input(self, pressed, held):
+        for every_key in pressed:
+            if every_key == pygame.K_w:
+                self.change_scene(MenuScene())
+
+    def update(self):
+        pass
+
+    def render(self, screen):
+        screen.fill((255, 255, 255))
+
 
 class Program:
 
     def __init__(self) -> None:
         self.running = True
-        pass
-    
-    def processInput(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-                break
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    self.running = False
-                    break
-                elif event.key == pygame.K_RIGHT:
-                    pass
-                elif event.key == pygame.K_LEFT:
-                    pass
-                elif event.key == pygame.K_DOWN:
-                    pass
-                elif event.key == pygame.K_UP:
-                    pass
-
-    def update(self):
-        pass
-
-    def render(self):
-
-        pygame.display.update()
-
-    def run(self):
+        
+    def run(self, width, height, current_scene):
+        """
+        self.scene = self.scene.this_scene updates the scene. If the scene is
+        changed, then this line will update it accordingly.
+        """
+        pygame.init()
+        screen = pygame.display.set_mode([width, height])
+        scene = current_scene
         while self.running:
-            self.processInput()
-            self.update()
-            self.render()
-        pass
+            keys_pressed = []
+            keys_held = pygame.key.get_pressed()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    # Quit condition if you press the X on the top right
+                    self.running = False
+                    scene.run_scene = False
+                if event.type == pygame.KEYDOWN:
+                    keys_pressed.append(event.key)
 
-program = Program()
+            if not scene.run_scene:
+                # Stop the game using other conditions
+                scene.close_game()
+                self.running = False
+            else:
+                scene.input(keys_pressed, keys_held)
+                scene.update()
+                scene.render(screen)
+                scene = scene.this_scene
+
+            pygame.display.update()
+
+
 if __name__ == "__main__":
-    program.run()
+    start_game = Program()
+    start_scene = MenuScene()
+    start_game.run(640, 480, start_scene)
+    pygame.quit()
