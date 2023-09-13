6b02cf52ebeee5ae621618f0024521dcecef8c88
Steven <47337807+slysnivy@users.noreply.github.com>
2023-09-13 18:55:37 -0400

    main v.0.2.9

--- dont stop now/main.py
+++ dont stop now/main.py
@@ -1,6 +1,6 @@
 import pygame
-import dsn_levels as dsnlevel
-import dsn_class as dsnclass
+import kog_levels as koglevel
+import kog_class as kogclass
 
 
 class Program:
@@ -9,12 +9,12 @@ class Program:
     """
     def __init__(self, width, height) -> None:
         self.running = True     # Determines if the game is running
-        self.memory = dsnclass.Memory(width / 1080, height / 576)     # Initialize game memory
+        self.memory = kogclass.Memory(width / 1080, height / 576)     # Initialize game memory
         self.memory.load_all_levels()   # Load all levels from different files
 
         self.memory.init_replays()
         self.memory.load_save()
-        self.memory.music = dsnclass.Music(self.memory.total_music_per)
+        self.memory.music = kogclass.Music(self.memory.total_music_per)
 
     def run(self, width, height, current_scene):
         """
@@ -101,14 +101,14 @@ if __name__ == "__main__":
     game_height = 576
 
     file_path = "assets/images/"
-    pygame.display.set_caption("Dont Stop Now") # game window caption
+    pygame.display.set_caption("Keep On Going") # game window caption
     icon = pygame.image.load(file_path + "rect10.png") # loading image
     default_icon_image_size = (32, 32) # reducing size of image
     icon = pygame.transform.scale(icon, default_icon_image_size) # scaling image correctly
     pygame.display.set_icon(icon) # game window icon
 
     start_game = Program(game_width, game_height)      # Initialize running the game with Program
-    start_scene = dsnlevel.MenuScene(24, 303, start_game.memory)
+    start_scene = koglevel.MenuScene(24, 303, start_game.memory)
     # Initialize the first scene/starting scene shown to the player
     start_game.run(game_width, game_height, start_scene)  # Run the game loop
     """The game loop will be stuck at this line (start_game.run) until the
